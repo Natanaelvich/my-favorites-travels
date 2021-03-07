7c96e50cbcde5a418c6273d6d0833b5549825749
Natanael Lima <taelima1997@gmail.com>
2021-03-07 18:06:51 -0300

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,11 @@
-import SEO from '@/components/Seo';
 import { Container } from '../styles/SingnIn/styles';
 
 export default function Home() {
   return (
     <Container>
-      <SEO
-        title="Bem vindo"
-        description="Projeto integrador ADS 4º período Unifacema 2020."
-        shouldIndexPage
-        image="Seo.png"
-        shouldExcludeTitleSuffix={false}
-      />
       <h1>inicio</h1>
       <h2>Test</h2>
+      <h2>Test2</h2>
     </Container>
   );
 }
