8ee81b4fea94475d4380e09995d7b779b42cffa0
Natanael Lima <taelima1997@gmail.com>
2021-03-09 00:03:10 -0300

    remove unsed files

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import { AuthProvider } from './modules/AuthContext';
 import { ToastProvider } from './modules/ToastContext';
 
 const AppProvider: React.FC = ({ children }) => (
-  <AuthProvider>
-    <ToastProvider>{children}</ToastProvider>
-  </AuthProvider>
+  <ToastProvider>{children}</ToastProvider>
 );
 
 export default AppProvider;
--- src/services/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-import { urls } from '../constants';
-
-const api = axios.create({
-  baseURL: urls[process.env.NODE_ENV],
-});
-
-export default api;
