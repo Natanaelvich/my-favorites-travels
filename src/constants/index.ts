591b01090ce108c63469b5fbe418643c64c3bba5
Natanael Lima <taelima1997@gmail.com>
2021-03-14 01:16:52 -0300

    remove unsed files

--- src/constants/index.ts
+++
@@ -1,5 +0,0 @@
-export const urls = {
-  test: `http://localhost:3335`,
-  development: 'http://localhost:3335',
-  production: 'https://api.pi.mundotech.dev',
-};
--- src/hooks/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ToastProvider } from './modules/ToastContext';
-
-const AppProvider: React.FC = ({ children }) => (
-  <ToastProvider>{children}</ToastProvider>
-);
-
-export default AppProvider;
