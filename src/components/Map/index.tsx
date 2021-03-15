60add88f58116bd9e1ce069543fd78c418a423fd
Natanael Lima <taelima1997@gmail.com>
2021-03-14 21:46:25 -0300

    config map zoom

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,8 +1,9 @@
 import { useRouter } from 'next/dist/client/router';
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, MapConsumer } from 'react-leaflet';
 import L from 'leaflet';
 
 import * as S from './styles';
+import { mapView } from './config';
 
 const markerIcon = new L.Icon({
   iconUrl:
@@ -49,8 +50,8 @@ const Map = ({ places }: MapProps) => {
   return (
     <S.MapWrapper>
       <MapContainer
-        center={[0, 0]}
-        zoom={3}
+        center={mapView.center}
+        zoom={mapView.zoom}
         style={{ height: '100%', width: '100%' }}
         minZoom={3}
         maxBounds={[
@@ -58,6 +59,27 @@ const Map = ({ places }: MapProps) => {
           [180, -180],
         ]}
       >
+        <MapConsumer>
+          {map => {
+            const width =
+              window.innerWidth ||
+              document.documentElement.clientWidth ||
+              document.body.clientWidth;
+
+            if (width < 768) {
+              map.setMinZoom(2);
+            }
+
+            map.addEventListener('dragend', () => {
+              mapView.setView(map.getCenter());
+            });
+            map.addEventListener('zoomend', () => {
+              mapView.setView(map.getCenter(), map.getZoom());
+            });
+
+            return null;
+          }}
+        </MapConsumer>
         <CustomTileLayer />
 
         {places?.map(({ id, slug, name, location }) => {
