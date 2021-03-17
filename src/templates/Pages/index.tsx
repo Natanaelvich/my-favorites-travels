c48cca9350569694148b5804687a1506b8d9695d
Natanael Lima <taelima1997@gmail.com>
2021-03-16 23:30:05 -0300

    add seo tags in pages

--- src/templates/Pages/index.tsx
+++ src/templates/Pages/index.tsx
@@ -1,5 +1,6 @@
 import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline';
 import LinkWrapper from 'components/LinkWrapper';
+import { NextSeo } from 'next-seo';
 
 import * as S from './styles';
 
@@ -10,6 +11,17 @@ export type PageTemplateProps = {
 
 const PageTemplate = ({ heading, body }: PageTemplateProps) => (
   <S.Content>
+    <NextSeo
+      title={`${heading} - Travel`}
+      description="A simple project to show in a map the places that I went and show more informations and photos when clicked."
+      canonical="https://myfavoritetravels.vercel.app"
+      openGraph={{
+        url: 'https://myfavoritetravels.vercel.app',
+        title: `${heading} - My Facorite Travels`,
+        description:
+          'A simple project to show in a map the places that I went and show more informations and photos when clicked.',
+      }}
+    />
     <LinkWrapper href="/">
       <CloseOutline size={32} />
     </LinkWrapper>
